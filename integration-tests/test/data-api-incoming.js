4d9bebf809a0e67a81196454eda17d2488a72d0b
Alecs Geuder <al.geuder@gmail.com>
2014-12-26 12:26:41 +0100

    adaption to sec->msec parsing
    + small simplification to callback jungle regarding deletion of
      database on error

--- integration-tests/test/data-api-incoming.js
+++ integration-tests/test/data-api-incoming.js
@@ -6,6 +6,9 @@ var should = require('should')
 
 
 var FIX = {
+  httpTestApi: "http://localhost:8086/",
+  httpTestRoot: "root",
+  httpTestRootPwd: "root",
   sensorX: {
     "name": "SensorX",
     "columns": ["time", "line"],
@@ -30,11 +33,11 @@ var FIX = {
 
 FIX.sensorData = [FIX.sensorX, FIX.sensorY];
 FIX.sensorDataAsString = "" +
-  "(a " + new Date(2007,8,9,10).valueOf() + " 12) " +
-  "(a " + new Date(2007,8,9,11).valueOf() + " 11) " +
-  "(b " + new Date(2007,8,9,10,12,13).valueOf() + " 100.1 3) " +
-  "(b " + new Date(2007,8,9,11,12,14).valueOf() + " 1.12 4) " +
-  "(b " + new Date(2007,8,9,11,12,15).valueOf() + " 122.1 6)";
+  "(a " + Math.floor(new Date(2007,8,9,10).valueOf() / 1000) + " 12) " +
+  "(a " + Math.floor(new Date(2007,8,9,11).valueOf() / 1000)+ " 11) " +
+  "(b " + Math.floor(new Date(2007,8,9,10,12,13).valueOf() / 1000) + " 100.1 3) " +
+  "(b " + Math.floor(new Date(2007,8,9,11,12,14).valueOf() / 1000) + " 1.12 4) " +
+  "(b " + Math.floor(new Date(2007,8,9,11,12,15).valueOf() / 1000) + " 122.1 6)";
 
 
 // Compare actual to expected data
@@ -51,8 +54,8 @@ function checkForPoints(responseData, expectedData) {
 
 // Make a http get request to get and compare the data to what is expected.
 function queryDbForData(callback) {
-  var http_client = request.newClient( "http://localhost:8086/");
-  http_client.setBasicAuth('root', 'root');
+  var http_client = request.newClient(FIX.httpTestApi);
+  http_client.setBasicAuth(FIX.httpTestRoot, FIX.httpTestRootPwd);
 
   var prefix = FIX.punter.name + '_' + FIX.site;
 
@@ -70,31 +73,36 @@ function queryDbForData(callback) {
       checkForPoints(_.first(body), namespacedY);
 
       // make sure we can delete the db
-      http_client.setBasicAuth('root', 'root');
-      http_client.del("db/toilets", function(err, res, body){
-        res.should.have.property('statusCode', '204');
-        callback();
-      });
+      deleteDatabase(callback);
     });
   });
 }
 
-// The following function is abused as a high-level test that checks
-// whether one can (1) create a db, (2) add point series to it and
+function deleteDatabase(callback){
+  var http_client = request.newClient(FIX.httpTestApi);
+  http_client.setBasicAuth(FIX.httpTestRoot, FIX.httpTestRootPwd);
+  http_client.del("db/toilets", function(err, res, body){
+    res.should.have.property('statusCode', '204');
+    callback();
+  });
+}
+
+// High-level test for http api that checks whether one can
+// (1) create a database
+// (2) add point series to it
 // (3) delete it
-//
 describe('incoming_http', function() {
-  var http_client = request.newClient( "http://localhost:8086/");
-  http_client.setBasicAuth('root', 'root');
+  var http_client = request.newClient(FIX.httpTestApi);
+  http_client.setBasicAuth(FIX.httpTestRoot, FIX.httpTestRootPwd);
 
   it('should accept a http post and write the data', function(done) {
-
-    // make sure we can create a test database
-    http_client.post("db", {"name": "toilets"}, function(err, res, b){
+    http_client.post("db", {"name": "toilets"}, function(err, res, b) {
+      if (err) deleteDatabase(function(){ throw err });
       res.should.have.property('statusCode', '201');
-
       var userUrl = FIX.dbUrl + "/users";
       http_client.post(userUrl, FIX.punter, function(err, res, b) {
+        if (err) deleteDatabase(function(){ throw err });
+        res.should.have.property('statusCode', '200');
         // now newly created, for the rest set auth to normal user
         http_client.setBasicAuth(FIX.punter.name, FIX.punter.password);
 
@@ -104,32 +112,34 @@ describe('incoming_http', function() {
 
         var seriesUrl = FIX.dbUrl + "/series";
         http_client.post(seriesUrl, namespaced, function(err, res, b) {
+          if (err) deleteDatabase(function(){ throw err });
           res.should.have.property('statusCode', '200'); // shouldn't it be 201??
           queryDbForData(done);
         });
       });
-
     });
   });
-
-  after(function(){
-    http_client.setBasicAuth('root', 'root');
-    http_client.del("db/toilets", function(err, res, body){});
-  });
-
 });
 
+// High-level blackbox test for mqtt api that checks implicitely whether
+// submitted data is
+// (1) parsed correctly
+// (2) its checksum is verified correctly
+// (3) it is sent to the http dataserver which then is being checked for the
+//     existence of this data
 describe('incoming_mqtt', function() {
-  var http_client = request.newClient( "http://localhost:8086/");
-  http_client.setBasicAuth('root', 'root');
+  var http_client = request.newClient(FIX.httpTestApi);
+  http_client.setBasicAuth(FIX.httpTestRoot, FIX.httpTestRootPwd);
 
   it('should accept an mqtt publish by a known client and distribute the data', function(done) {
     // create a test database
     http_client.post("db", {"name": "toilets"}, function(err, res, body){
+      if (err) deleteDatabase(function(){ throw err });
+      res.should.have.property('statusCode', '201');
       http_client.post(FIX.dbUrl + "/users", FIX.punter, function(e, res, b) {
+        if (e) deleteDatabase(function(){ throw e });
         // now newly created, for the rest set auth to normal user
         http_client.setBasicAuth(FIX.punter.name, FIX.punter.password);
-        res.should.have.property('statusCode', '200');
 
         var mqtt_client = mqtt.createClient(1883, 'localhost', {
           clientId: FIX.site,
@@ -139,25 +149,18 @@ describe('incoming_mqtt', function() {
 
         var checksum = utils.checkchecksum(FIX.sensorDataAsString);
 
-        // We know (rather assume here) that the last thing the server does is
-        // to send out the verified Data message. Still, we check for the data
-        // in the database at the very end.
+        // We assume here that the last thing the server does is to send out
+        // the verified Data message. Still, we check for the data in the
+        // database at the very end.
         mqtt_client.on('message', function(toppic, message) {
           toppic.should.equal('verifiedData');
           message.should.equal(checksum);
           mqtt_client.end();
           queryDbForData(done);
         });
-
         mqtt_client.publish(checksum, FIX.sensorDataAsString);
       });
     });
   });
-
-  after(function(){
-    http_client.setBasicAuth('root', 'root');
-    http_client.del("db/toilets", function(err, res, body){});
-  });
-
 });
 
